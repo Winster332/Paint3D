7635d7bd9443820549ea82da24561df3b3dbacc6
Stas Martynov <stas-martynov-98@mail.ru>
2016-09-27 01:01:38 +0300

    Добавлен интерфейс кнопки

--- Paint3D/Core/UI/GButtonFlat.cs
+++ Paint3D/Core/UI/GButtonFlat.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace Paint3D.Core.UI
 {
-	public class GButtonFlat
+	public class GButtonFlat : Button
 	{
+		public GButtonFlat(Control parent)
+		{
+			parent.Controls.Add(this);
+			this.FlatStyle = FlatStyle.Flat;
+			this.FlatAppearance.BorderSize = 0;
+			this.BackColor = Color.FromArgb(0, 140, 200);
+			this.Text = "Button";
+			this.Font = new Font("Calibri", 10);
+			this.ForeColor = Color.FromArgb(250,250,250);
+		}
 	}
 }
