2f6d99976ade0a7f0e0931633912a306a0a2e012
Stas Martynov <stas-martynov-98@mail.ru>
2016-09-27 02:18:24 +0300

    Описана работа мира

--- Paint3D/Core/GWorld.cs
+++ Paint3D/Core/GWorld.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Paint3D.Core.Models;
 
 namespace Paint3D.Core
 {
 	public class GWorld
 	{
+		private GEngine engine;
+		private List<GBaseModel> models;
+		public GWorld(GEngine engine)
+		{
+			this.engine = engine;
+			this.models = new List<GBaseModel>();
+		}
+		public void Draw()
+		{
+			for (int i = 0; i < models.Count; i++)
+				models[i].Draw();
+		}
+		public void AddBox(float x, float y, float z, float rad, float r, float g, float b)
+		{
+			models.Add(new GBox(engine, x, y, z, rad, r, g, b));
+		}
+		public void AddSphere(float x, float y, float z, float rad, float r, float g, float b)
+		{
+			models.Add(new GSphere(engine, x, y, z, rad, r, g, b));
+		}
 	}
 }
