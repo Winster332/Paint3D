d833c799cc19fc3b020ddc7f61e594dea390b174
Stas Martynov <stas-martynov-98@mail.ru>
2016-09-27 00:42:55 +0300

    Revert "Revert "Добавлены классы для удобного представления данных""
    This reverts commit fc63cfa1d25a0a09696d41cc0a0dfd9ab02f570b.

--- Paint3D/Core/GEngine.cs
+++ Paint3D/Core/GEngine.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿////////////////////////////
+/// Главный класс который предоставляет API
+/// для всего пространства имен Core
+////////////////////////////
+
+using System;
 
 namespace Paint3D.Core
 {
 	public class GEngine
 	{
+		private System.Windows.Forms.Control parent;
+        private System.Windows.Forms.Timer update;
+		private GGraphics graphics;
+		private GManagerFile managerFile;
+		private GLogger logger;
+		/// <summary>
+		/// Создает объект класса GEngine
+		/// </summary>
+		/// <param name="parent">Parent элемент в котором создается GEngine</param>
+		public GEngine(System.Windows.Forms.Control parent)
+		{
+			this.parent = parent;
+			graphics = new GGraphics();
+			managerFile = new GManagerFile();
+			logger = new GLogger();
+
+			update = new System.Windows.Forms.Timer();
+			update.Interval = 30;
+			update.Tick += Update_Tick;
+		}
+		public void Run()
+		{
+			update.Start();
+		}
+		private void Update_Tick(object sender, EventArgs e)
+		{
+			// TODO: Need write loop function
+		}
+
+		#region GET
+		public GLogger GetLogger()
+		{
+			return logger;
+		}
+		public GManagerFile GetManagerFile()
+		{
+			return managerFile;
+		}
+		public GGraphics GetGraphics()
+		{
+			return graphics;
+		}
+		#endregion
 	}
 }
