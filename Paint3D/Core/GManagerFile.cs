fc63cfa1d25a0a09696d41cc0a0dfd9ab02f570b
Stas Martynov <stas-martynov-98@mail.ru>
2016-09-27 00:41:33 +0300

    Revert "Добавлены классы для удобного представления данных"
    This reverts commit fc31217f756e07bb644eb13b06d083d7c117023a.

--- Paint3D/Core/GManagerFile.cs
+++ Paint3D/Core/GManagerFile.cs
@@ -1,59 +1,12 @@
-﻿////////////////////////////
-/// Класс для сериализации объектов
-////////////////////////////
-
-using System;
-using System.Xml.Serialization;
-using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Paint3D.Core
 {
 	public class GManagerFile
 	{
-		/// <summary>
-		/// Путь к сохраненным файлам
-		/// </summary>
-		public const String PATH_DATA = @"Data\";
-		/// <summary>
-		/// Сериализует объект
-		/// </summary>
-		/// <param name="name">Имя файла</param>
-		/// <param name="obj">Объект</param>
-		public void SetDataXMLFile(string name, object obj)
-		{
-			String all_path = PATH_DATA + name + "//" + name + ".xml";
-			XmlSerializer serializer = new XmlSerializer(obj.GetType());
-
-			if (!Directory.Exists(PATH_DATA))
-				Directory.CreateDirectory(PATH_DATA);
-			Directory.CreateDirectory(name);
-
-			using (Stream stream = new FileStream(all_path, FileMode.Create))
-			{
-				serializer.Serialize(stream, obj);
-			}
-		}
-		/// <summary>
-		/// Десериализует объект
-		/// </summary>
-		/// <param name="name">Имя файла</param>
-		/// <param name="type">Тип объекта</param>
-		/// <returns></returns>
-		public object GetDataXMLFile(string name, Type type)
-		{
-			String all_path = PATH_DATA + name + "//" + name +  ".xml";
-			XmlSerializer serializer = new XmlSerializer(type);
-			Object obj_res = null;
-
-			if (!Directory.Exists(PATH_DATA))
-				Directory.CreateDirectory(PATH_DATA);
-
-			using (Stream stream = new FileStream(all_path, FileMode.Open))
-			{
-				obj_res = serializer.Deserialize(stream);
-			}
-
-			return obj_res;
-		}
 	}
 }
