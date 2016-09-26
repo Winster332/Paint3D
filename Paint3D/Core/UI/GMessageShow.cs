284d659ac2990cabc661cd520d3b613657867069
Stas Martynov <stas-martynov-98@mail.ru>
2016-09-27 02:57:49 +0300

    Описан класс GMessageShow

--- Paint3D/Core/UI/GMessageShow.cs
+++ Paint3D/Core/UI/GMessageShow.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,57 @@ using System.Threading.Tasks;
 
 namespace Paint3D.Core.UI
 {
-	public class GMessageShow
+	public class GMessageShow : Panel
 	{
+		private Label label;
+		private Timer update;
+		private int ticks = 50;
+		private bool isShow = false;
+		public GMessageShow(Control parent)
+		{
+			parent.Controls.Add(this);
+			label = new Label();
+			this.Width = 300;
+			this.Height = 30;
+			this.Controls.Add(label);
+			this.BackColor = Color.FromArgb(192,192,192);
+			this.label.Text = "Test text";
+			this.label.ForeColor = Color.FromArgb(100, 100, 100);
+			this.label.Width = this.Width;
+			this.label.Font = new Font("Arial", 14);
+			this.label.Location = new Point(10, 4);
+			this.Left = parent.Width / 2 - this.Width / 2;
+			this.Top = -this.Height;
+
+			update = new Timer();
+			update.Interval = 30;
+			update.Tick += (o, e) =>
+			{
+				if (isShow)
+				{
+					if (this.Top < 0)
+						this.Top += 1;
+					else
+					{
+						if (ticks > 0)
+							ticks--;
+						else isShow = false;
+					}
+				}
+				else
+				{
+					if (this.Top > -this.Height)
+						this.Top -= 1;
+					else update.Stop();
+				}
+			};
+		}
+		public void ShowMessage(string text)
+		{
+			ticks = 50;
+			isShow = true;
+			label.Text = text;
+			update.Start();
+		}
 	}
 }
