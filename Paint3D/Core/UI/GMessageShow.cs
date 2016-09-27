d33ad5ca8937ac4f9db38855bbf06af104d01f81
Stas Martynov <stas-martynov-98@mail.ru>
2016-09-27 20:36:19 +0300

    Незначительные доработки

--- Paint3D/Core/UI/GMessageShow.cs
+++ Paint3D/Core/UI/GMessageShow.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Drawing;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Paint3D.Core.UI
 {
