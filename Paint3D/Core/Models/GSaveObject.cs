2ba1426692c7dfd95b173c74c8e0e050c341f86a
Stas Martynov <stas-martynov-98@mail.ru>
2016-09-27 22:19:45 +0300

    Добавлен объект для хранения карт

---
+++ Paint3D/Core/Models/GSaveObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paint3D.Core.Models
+{
+	[Serializable]
+	public class GSaveObject
+	{
+		public enum TYPE { Cube, Sphere };
+		public TYPE type;
+		public float x;
+		public float y;
+		public float z;
+		public float rad;
+		public float r;
+		public float g;
+		public float b;
+	}
+}
